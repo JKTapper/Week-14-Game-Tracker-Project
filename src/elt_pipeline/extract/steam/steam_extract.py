297dd9580d296ef546667928d009eb93ded6e360
james-plw <james.wright2@bath.edu>
2025-07-29 16:44:33 +0100

    Working singular extract file

--- src/elt_pipeline/extract/steam/steam_extract.py
+++ src/elt_pipeline/extract/steam/steam_extract.py
@@ -2,7 +2,7 @@
 PC games and stores all scraped games in a json list.
 Takes json list of newly scraped games, requests data from API
 and adds supplementary data to the json list.'''
-import json
+# import json
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import requests
@@ -45,16 +45,62 @@ def parse_games_bs(html):
     for a_tag in a_tags:
         link = a_tag.get('href')
         if 'https://store.steampowered.com/app' in link:
+            app_id = a_tag.get("data-ds-appid")
             title = a_tag.find('span').get_text()
             release_date = a_tag.find(
                 'div', class_='col search_released responsive_secondrow').get_text().strip()
             game = {
                 'url': link,
+                'app_id': app_id,
                 'title': title,
                 'release': release_date
             }
             games.append(game)
     return games
+
+
+def get_steam_game_details(app_id: int) -> dict[str]:
+    '''Takes Steam store app ID and requests data through the API
+    Returns dict of useful data'''
+    url = f"https://store.steampowered.com/api/appdetails?appids={app_id}"
+    try:
+        response = requests.get(url, timeout=10)
+        if response.status_code == 200:
+            data = response.json()
+
+            if data.get(str(app_id), {}).get('success'):
+                game_data = data[str(app_id)]['data']
+                return {
+                    'app_id': app_id,
+                    'publishers': game_data.get('publishers', []),
+                    'developers': game_data.get('developers', []),
+                    'description': game_data.get('short_description'),
+                    'requirements': game_data.get('pc_requirements'),
+                    'is_free': game_data.get('is_free', False),
+                    'price': game_data.get('price_overview', {}).get('final'),
+                    'genres': [genre['description'] for genre in game_data.get('genres', [])],
+                }
+            return {'app_id': app_id, 'error': 'Data not found or incomplete'}
+        return {'app_id': app_id,
+                'error': f"HTTP error {response.status_code}: {response.reason}"}
+    except requests.RequestException as e:
+        return {'app_id': app_id, 'error': f'Request failed: {str(e)}'}
+
+
+def iterate_through_scraped_games(json_data: list[dict[str]]):
+    '''
+    Iterates through list of games and adds on supplementary data into each dict
+    '''
+    games_full_data = []
+    for item in json_data:
+        app_id = item['app_id']
+        if app_id:
+            details = get_steam_game_details(app_id)
+            # Merge original data with extra info
+            full_data = item | details
+            print(full_data)
+            games_full_data.append(full_data)
+    return games_full_data
 
 
 if __name__ == "__main__":
@@ -63,5 +109,9 @@ if __name__ == "__main__":
     steam_html = get_html(STEAM_URL)
     scraped_games = parse_games_bs(steam_html)[0:5]  # first 5 only
     new_games = [
-        new_game for new_game in scraped_games if str(new_game.get("id")) not in existing_games]
+        new_game for new_game in scraped_games if str(new_game.get("app_id")) not in existing_games]
     print(new_games)
+
+    full_game_data = iterate_through_scraped_games(new_games)
+    print('/n/n')
+    print(full_game_data)
