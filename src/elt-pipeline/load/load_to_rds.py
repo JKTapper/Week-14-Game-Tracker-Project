385e53ba594c131d593b610740978337384f67f5
Nabeel <78046068+nab02@users.noreply.github.com>
2025-07-29 10:04:47 +0100

    Update load_to_rds.py

--- src/elt-pipeline/load/load_to_rds.py
+++ src/elt-pipeline/load/load_to_rds.py
@@ -1,5 +1,37 @@
 """Script to take transformed data and stores into our RDS Postgres Database"""
-
+import os
+from dotenv import load_dotenv
 import pandas as pd
+from sqlalchemy import create_engine, text, engine
 
+load_dotenv()
+AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_PORT = int(os.getenv("DB_PORT"))
+DB_SCHEMA = os.getenv("DB_SCHEMA")
+
+
+def get_engine() -> engine:
+    """Connects to RDS instance"""
+    engine = create_engine(
+        f"mssql+pymssql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    )
+    return engine
+
+
+def load_data_into_database():
+    ...
+
+
+def main() -> None:
+    ...
+
+
+if __name__ == "__main__":
+    main()
 #  Data coming in will be a pandas dataframe
