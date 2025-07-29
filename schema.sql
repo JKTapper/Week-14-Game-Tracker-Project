1207263ac6615e6c8b2212ace2d7efecc32904e6
claudio <claudiolou97@gmail.com>
2025-07-29 12:25:13 +0100

    Updated schema

--- schema.sql
+++ schema.sql
@@ -2,6 +2,8 @@
 DROP TABLE IF EXISTS video;
 DROP TABLE IF EXISTS image;
 DROP TABLE IF EXISTS genre_assignment;
+DROP TABLE IF EXISTS developer_assignment;
+DROP TABLE IF EXISTS publisher_assignment;
 DROP TABLE IF EXISTS game;
 DROP TABLE IF EXISTS genre;
 DROP TABLE IF EXISTS publisher;
@@ -10,58 +12,72 @@ DROP TABLE IF EXISTS developer;
 -- Create publisher
 CREATE TABLE publisher (
     publisher_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    publisher_name TEXT UNIQUE NOT NULL
+    publisher_name TEXT NOT NULL
 );
 
 -- Create developer
 CREATE TABLE developer (
     developer_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    developer_name TEXT UNIQUE NOT NULL
-);
-
--- Create game 
-CREATE TABLE game (
-    game_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    game_name TEXT UNIQUE NOT NULL,
-    release_date DATE,
-    publisher_id INT,
-    developer_id INT,
-    game_description TEXT,
-    recent_reviews_summary TEXT,
-    os_requirements TEXT,
-    storage_requirements TEXT,  
-    price FLOAT,
-    FOREIGN KEY (publisher_id) REFERENCES publisher (publisher_id),
-    FOREIGN KEY (developer_id) REFERENCES developer (developer_id)
+    developer_name TEXT NOT NULL
 );
 
 -- Create genre
 CREATE TABLE genre (
     genre_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    genre_name TEXT UNIQUE NOT NULL
+    genre_name TEXT NOT NULL
 );
 
--- Create genre_assignment
+-- Create game
+CREATE TABLE game (
+    game_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    game_name TEXT UNIQUE NOT NULL,
+    release_date DATE,
+    game_description TEXT,
+    recent_reviews_summary TEXT,
+    os_requirements TEXT,
+    storage_requirements TEXT,
+    price FLOAT
+);
+
+-- Game-Genre relationship
 CREATE TABLE genre_assignment (
     genre_assignment_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    genre_id INT,
-    game_id INT,
+    genre_id INT NOT NULL,
+    game_id INT NOT NULL,
     FOREIGN KEY (genre_id) REFERENCES genre (genre_id),
     FOREIGN KEY (game_id) REFERENCES game (game_id)
 );
 
--- Create image
+-- Game-Developer relationship
+CREATE TABLE developer_assignment (
+    developer_assignment_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    developer_id INT NOT NULL,
+    game_id INT NOT NULL,
+    FOREIGN KEY (developer_id) REFERENCES developer (developer_id),
+    FOREIGN KEY (game_id) REFERENCES game (game_id)
+);
+
+-- Game-Publisher relationship
+CREATE TABLE publisher_assignment (
+    publisher_assignment_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    publisher_id INT NOT NULL,
+    game_id INT NOT NULL,
+    FOREIGN KEY (publisher_id) REFERENCES publisher (publisher_id),
+    FOREIGN KEY (game_id) REFERENCES game (game_id)
+);
+
+-- Game Images
 CREATE TABLE image (
     image_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    url_image TEXT UNIQUE NOT NULL,
+    url_image TEXT,
     game_id INT,
     FOREIGN KEY (game_id) REFERENCES game (game_id)
 );
 
--- Create video
+-- Game Videos
 CREATE TABLE video (
     video_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    url_video TEXT UNIQUE NOT NULL,
+    url_video TEXT,
     game_id INT,
     FOREIGN KEY (game_id) REFERENCES game (game_id)
 );
