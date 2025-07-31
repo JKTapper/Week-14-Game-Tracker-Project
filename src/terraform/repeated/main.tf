05791824449e3d1ade4ff9744704465c4580a9bf
claudio <claudiolou97@gmail.com>
2025-07-31 19:47:44 +0100

    Added tf for LT lambda

--- src/terraform/repeated/main.tf
+++ src/terraform/repeated/main.tf
@@ -71,7 +71,7 @@ resource "aws_lambda_function" "docker_lambda" {
 # Schedule (every hour)
 resource "aws_cloudwatch_event_rule" "hourly" {
   name                = "c18-game-tracker-lambda-el-hourly-schedule"
-  schedule_expression = "rate(1 hour)"
+  schedule_expression = "cron(0 * * * ? *)"
 }
 
 # Permission for EventBridge to invoke Lambda
@@ -90,6 +90,53 @@ resource "aws_cloudwatch_event_target" "lambda_target" {
   arn       = aws_lambda_function.docker_lambda.arn
 }
 
+
+# LT lambda
+resource "aws_lambda_function" "docker_lambda_lt" {
+  function_name = "c18-game-tracker-lt-lambda"
+  package_type  = "Image"
+  image_uri     = var.lambda_image_uri # Assumes same image
+  role          = aws_iam_role.lambda_exec_role_game_tracker_el.arn
+  timeout       = 60
+  memory_size   = 512
+
+  environment {
+    variables = {
+      LOG_LEVEL   = "INFO"
+      DB_HOST     = var.DB_HOST
+      DB_PORT     = var.DB_PORT
+      DB_USER     = var.DB_USER
+      DB_PASSWORD = var.DB_PASSWORD
+      DB_NAME     = var.DB_NAME
+      DB_SCHEMA   = "public"
+    }
+  }
+}
+
+
+resource "aws_cloudwatch_event_rule" "lt_schedule" {
+  name                = "c18-game-tracker-lambda-lt-schedule"
+  schedule_expression = "cron(30 * * * ? *)"
+}
+
+resource "aws_lambda_permission" "lt_permission" {
+  statement_id  = "AllowExecutionFromEventBridgeLT"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.docker_lambda_lt.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.lt_schedule.arn
+}
+
+resource "aws_cloudwatch_event_target" "lt_target" {
+  rule      = aws_cloudwatch_event_rule.lt_schedule.name
+  target_id = "lambda-lt"
+  arn       = aws_lambda_function.docker_lambda_lt.arn
+}
+
+
+
+
+
 # Existing data
 data "aws_ecs_cluster" "existing" {
   cluster_name = var.ecs-cluster-name
@@ -98,23 +145,23 @@ data "aws_ecr_repository" "repo" {
   name = var.ecr_repo_name
 }
 data "aws_vpc" "c18-vpc" {
-  id    = "vpc-0adcb6a62ca552c01"
+  id = "vpc-0adcb6a62ca552c01"
 }
 data "aws_subnet" "public_1" {
-  id = "subnet-0679d4b1f9e7839ef"  
+  id = "subnet-0679d4b1f9e7839ef"
 }
 data "aws_subnet" "public_2" {
-  id = "subnet-0f10662561eade8c3"  
+  id = "subnet-0f10662561eade8c3"
 }
 data "aws_subnet" "public_3" {
-  id = "subnet-0aed07ac008a10da9"  
+  id = "subnet-0aed07ac008a10da9"
 }
 
 # ECS security group and rules
 resource "aws_security_group" "ecs_sg" {
-    name = "c18-game-tracker-ecs-sg"
-    description = "Allows all outbound traffic from ECS."
-    vpc_id = data.aws_vpc.c18-vpc.id
+  name        = "c18-game-tracker-ecs-sg"
+  description = "Allows all outbound traffic from ECS."
+  vpc_id      = data.aws_vpc.c18-vpc.id
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all" {
@@ -156,15 +203,15 @@ resource "aws_ecs_task_definition" "service" {
       memory    = 1024
       essential = true
       environment = [
-  { name = "DB_HOST", value = var.DB_HOST },
-  { name = "DB_PORT", value = var.DB_PORT },
-  { name = "DB_USER", value = var.DB_USER },
-  { name = "DB_PASSWORD", value = var.DB_PASSWORD },
-  { name = "DB_NAME", value = var.DB_NAME },
-  { name = "DB_SCHEMA", value = var.DB_SCHEMA }
-]
-   
-    logConfiguration = {
+        { name = "DB_HOST", value = var.DB_HOST },
+        { name = "DB_PORT", value = var.DB_PORT },
+        { name = "DB_USER", value = var.DB_USER },
+        { name = "DB_PASSWORD", value = var.DB_PASSWORD },
+        { name = "DB_NAME", value = var.DB_NAME },
+        { name = "DB_SCHEMA", value = var.DB_SCHEMA }
+      ]
+
+      logConfiguration = {
         logDriver = "awslogs"
         options = {
           awslogs-group         = "/ecs/c18-game-tracker-dashboard-task"
@@ -184,7 +231,7 @@ resource "aws_ecs_service" "dashboard_service" {
   launch_type     = "FARGATE"
 
   network_configuration {
-    subnets          = [
+    subnets = [
       data.aws_subnet.public_1.id,
       data.aws_subnet.public_2.id,
       data.aws_subnet.public_3.id
