28a829d9109c880ba51a21cf05370ed3e9920d7a
Nabeel <78046068+nab02@users.noreply.github.com>
2025-07-31 09:45:33 +0100

    Update load_to_rds.py

--- src/elt_pipeline/load/load_to_rds.py
+++ src/elt_pipeline/load/load_to_rds.py
@@ -16,7 +16,7 @@ DB_HOST = os.getenv("DB_HOST")
 DB_NAME = os.getenv("DB_NAME")
 DB_USERNAME = os.getenv("DB_USERNAME")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_PORT = int(os.getenv("DB_PORT"))
+DB_PORT = int(os.getenv("DB_PORT", 5432))
 
 
 def get_engine() -> Engine:
