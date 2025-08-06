367d9db8cd26a930c3870224f637e361bd4c652a
nathduc <nducuru@yahoo.co.uk>
2025-08-06 09:51:35 +0100

    set up global 7 day variable

--- src/subscriber/notification.py
+++ src/subscriber/notification.py
@@ -8,13 +8,12 @@ from form import connect_to_rds
 
 def get_sub_notifications():
     """
-    Query RDS subcribers and the genres they want email notifications
+    Query RDS subscribers and the genres they want email notifications
     """
     connection = connect_to_rds()
     with connection.begin() as conn:
         print(pd.read_sql("""select * from subscriber_genre_assignment
         where email_notifications = true""", conn))
-
 
 
 if __name__ == "__main__":
