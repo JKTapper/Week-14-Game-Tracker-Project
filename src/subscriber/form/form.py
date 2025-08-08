afed89781beb3750ae7b62a36d752d870b5f9ed7
claudio <claudiolou97@gmail.com>
2025-08-08 12:08:59 +0100

    lint

--- src/subscriber/form/form.py
+++ src/subscriber/form/form.py
@@ -3,13 +3,13 @@ Flask app to run form for user subscriptions
 """
 import os
 import logging
+from typing import List, Union
 import pandas as pd
 from dotenv import load_dotenv
 from sqlalchemy import create_engine, text
 from sqlalchemy.engine import Engine, Connection
 from flask import Flask, render_template, request, Response
 import boto3
-from typing import List, Union
 from notification import handler
 
 logging.basicConfig(level=logging.INFO)
@@ -17,7 +17,7 @@ logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 
-VALID_GENRES = ('Casual', 'Indie', 'Simulation', 'Free To Play', 
+VALID_GENRES = ('Casual', 'Indie', 'Simulation', 'Free To Play',
                 'Adventure', 'Early Access', 'Acition', 'Strategy', 
                 'Massively Multiplayer', 'RPG', 'Sports', 'Racing',
                 'Exploration', 'Turn-based', 'Survival', 'Sci-fi', 
@@ -33,8 +33,9 @@ def verify_email(email: str) -> None:
     ses.verify_email_identity(
         EmailAddress=email
     )
-    return (f"Verification email sent to {email}. Check your inbox and click the link to complete verification.")
-    
+    return f"""Verification email sent to {email}.
+    Check your inbox and click the link to complete verification."""
+
 
 def connect_to_rds() -> Engine:
     """
@@ -51,7 +52,8 @@ def connect_to_rds() -> Engine:
     return create_engine(db_url)
 
 
-def insert_sub_genre_assignment(conn: Connection, subscriber_id: int, genres_sub: List[str]) -> None:
+def insert_sub_genre_assignment(conn: Connection,
+    subscriber_id: int, genres_sub: List[str]) -> None:
     """
     Insert genre assignments for a subscriber.
     """
@@ -77,7 +79,8 @@ def form() -> Union[str, Response]:
     connection = connect_to_rds()
 
     with connection.begin() as conn:
-        genres = pd.read_sql(f"SELECT * FROM genre where genre_name in {VALID_GENRES};", conn)['genre_name']
+        genres = pd.read_sql(f"""SELECT * FROM genre where
+        genre_name in {VALID_GENRES};""", conn)['genre_name']
 
     if request.method == "POST":
         email = request.form["email"]
@@ -87,7 +90,7 @@ def form() -> Union[str, Response]:
 
         try:
             with connection.begin() as conn:
-                
+
                 # If email exists delete the data
                 conn.execute(
                         text("""delete from subscriber where subscriber_email = :email"""),
@@ -108,10 +111,10 @@ def form() -> Union[str, Response]:
                 logger.info(f"Inserted subscriber {subscriber_id} with email {email}")
                 if genres_sub:
                     insert_sub_genre_assignment(conn, subscriber_id, genres_sub)
-        
+
             if email == "claudiolou97@gmail.com":
-                    handler(1,1)
-                    return f"We will contact you at {email}"
+                handler(1,1)
+                return f"We will contact you at {email}"
 
             return verify_email(email)
 
@@ -142,7 +145,7 @@ def unsub_form() -> Union[str, Response]:
             logger.error("Error unsubscribing:", e)
             return "Email doesn't exist"
 
-    
+
     return render_template("unsub.html")
 
 
